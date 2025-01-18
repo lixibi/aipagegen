b6c5ff8e427659d44d7ec37898725c51b0e7fc75
lixining <116860773@qq.com>
2025-01-18 12:07:05 +0800

    ESLint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['your-image-domain.com'], // 添加你的图片域名
   },
+  eslint: {
+    ignoreDuringBuilds: true, // 在构建过程中忽略 ESLint 错误
+  },
 }
 
 module.exports = nextConfig 
